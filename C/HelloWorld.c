cf982528580750350b2979cab620789b4c59fcc3
Abraranan18 <78102658+Abraranan18@users.noreply.github.com>
2022-05-19 02:58:40 +0600

    Add files via upload

---
+++ C/HelloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+
+        printf("Hello World\n");  //Here We use BackSlash to create a newline
+
+}
---
+++ Java/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{                 //WE CAN GIVE ANY CLASS NAME BUT EXCEPT THAT EVERYTHING SHOULD BE SAME 
+	public static void main(String[] args){
+		System.out.println("Hello world");
+	}
+}
